fd74fced6ddcb3bd5e89239edf3359def1f69332
alejandrardecajal <alejandrardecajal@gmail.com>
2022-10-08 22:28:24 -0300

    Segundo intento para restaurar commit de Facu
    Commit al que intento volver:
    commit 5b5a52344bfcbee4d0e40a80da16d54e86843c22

---
+++ proyecto-vue/app-web/src/App.vue
@@ -0,0 +1,39 @@
+node <template>
+  <div id="app">
+    <App_Homepage />
+    <app_section_1 />
+    <app_section_2 />
+    <app_section_3 /> 
+    <app_section_4 />
+  </div>
+</template>
+
+<script>
+import App_Homepage from './components/app_homepage.vue'
+import app_section_1 from './components/app_section_1.vue'
+import app_section_2 from './components/app_section_2.vue'
+import app_section_3 from './components/app_section_3.vue'
+import app_section_4 from './components/app_section_4.vue'
+
+export default {
+  name: 'app',
+  components: {
+    App_Homepage,
+    app_section_1,
+    app_section_2,
+    app_section_3,
+    app_section_4
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
