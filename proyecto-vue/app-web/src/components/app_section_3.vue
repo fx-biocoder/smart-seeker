c1a89909da7acc70e36283871c5c6fc607dde285
alejandrardecajal <alejandrardecajal@gmail.com>
2022-10-23 20:35:08 -0300

    Section3 falta foto Facu (en carpeta assets y actualizar link)

--- proyecto-vue/app-web/src/components/app_section_3.vue
+++ proyecto-vue/app-web/src/components/app_section_3.vue
@@ -7,17 +7,23 @@
             </div>
             <article class="team">
                 <div class="card">
-                    <img src="https://via.placeholder.com/100" alt="Facu">
+                    <img src="https://via.placeholder.com/200x227" alt="Facu">
                     <h4>Facundo Martinez</h4>
-                    <h5>Titulo</h5>
+                    <h5>Data Analyst | Python Developer</h5>                    
+                    <h5>Bioinformatician</h5>
+                    <a href="https://www.linkedin.com/in/fxmartinez/" target="_blank" id="lin">
+                        <img src="@/assets/linkedin.png"  alt="linkedin"></a>
 
                 </div>
 
                 <div class="card">
-                    <img src="https://via.placeholder.com/100" alt="Ale">
+                    <img src="@/assets/ale.png" alt="Ale">
                     <h4>Alejandra R. de Cajal</h4>
-                    <h5>Titulo</h5>
-
+                    <h5>Fullstack Developer Jr. | Food Technologist</h5>
+                    <h5>Project Management</h5>                                      
+                    <a href="https://www.linkedin.com/in/alejandra-r-de-cajal/" target="_blank" id="lin">
+                        <img src="@/assets/linkedin.png"  alt="linkedin"></a>
+                    
                 </div>
             </article>
 
@@ -44,26 +50,57 @@
     box-shadow: 0 10px 10px rgb(201, 200, 200);
     background-image: none;
 }
+
 .titulo {
     text-align: center;
     margin: 2%;
     padding: 1%;
 }
+
 /*Si modifico la imagen de esta sección, se modifica tambien el logo!! */
-img {
+.card img {
+
+    width: 15vh;
     border-radius: 50%;
-    /* padding: 20%; */
-    width: 100px;
+    /* background-size: 100% auto; */
 }
+
+/* .card a {
+    display: flex;
+    width: 20%;
+    align-self: center;
+
+} */
+
 .team {
     display: flex;
     flex-direction: row;
     margin: 2%;
     justify-content: space-around;
 }
-h4, h5 {
+
+.card {
+    display: flex;
+    flex-flow: column wrap;
+    width: 50%;
+}
+
+h4,
+h5,
+img {
     margin: 2%;
     padding: 1%;
+    width: max-content;
+    align-self: center;
+    font-weight: 600;
+}
+
+#lin{
+    display: contents;
    
+}
+
+.card a img{
+    width: 5vh;
 }
 </style>
