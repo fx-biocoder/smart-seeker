f9de304ac4a3c582e1ebdb4ba57c520b9067c699
Facundo Martínez <90737264+fx-biocoder@users.noreply.github.com>
2022-10-28 16:46:14 -0300

    Cambios grandes en estilo
    Fuentes de Google, cambios en la seccion de carga de archivos, cambios en la sección donde se explica el funcionamiento de la app, cambios en el formato de las tarjetas de miembros

--- src/components/app_navigation.vue
+++ src/components/app_navigation.vue
@@ -136,7 +136,7 @@ nav {
         align-items: center;
 
         img {
-            width: 50px;
+            width: 100px;
             transition: .5s ease all;
             
         }
@@ -209,7 +209,7 @@ nav {
     nav {
         padding: 8px 0;
         img {
-            width: 40px;
+            /*width: 70px;*/
             box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0,0,0,0.06);
         }
     }
